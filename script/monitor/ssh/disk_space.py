6a64edb4824ea9ff3bdd6d3c58e82b25250e2b6d
gk <shanghai_sl@hotmail.com>
2017-02-14 18:13:30 +0800

    Update disk_space.py

--- script/monitor/ssh/disk_space.py
+++ script/monitor/ssh/disk_space.py
@@ -2,13 +2,16 @@ import sys
 import json
 import re
 
+# Filesystem Size Used Avail Use% Mounted on
+
 f = open(sys.argv[1], "r")
 x = f.read()
 j = json.loads(x)
 
 
 def split_str(y):
-    return re.split(r"\s+", y)
+    row = re.split(r"\s+", y)
+    return {"Filesystem": row[0], "Size": row[1], "Used": row[2], "Avail": row[3], "Ued%": row[4], "Mounted on": row[5]}
 
 g = list(map(split_str, j))
 
