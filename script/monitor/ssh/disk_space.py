029faf59b9bd149f562591845fcf581e8f362914
gk <shanghai_sl@hotmail.com>
2017-02-14 17:46:58 +0800

    Update disk_space.py

--- script/monitor/ssh/disk_space.py
+++ script/monitor/ssh/disk_space.py
@@ -1,4 +1,15 @@
+import sys
+import json
+import re
 
-a
-a
-a
+f = open(sys.argv[1], "r")
+x = f.read()
+j = json.loads(x)
+
+
+def split_str(y):
+    return re.split(r"\s+", y)
+
+g = list(map(split_str, j))
+
+print(g)
