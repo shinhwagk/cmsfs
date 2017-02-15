83129847b937fb46462c08a2ae9b1eb04da33c09
gk <shanghai_sl@hotmail.com>
2017-02-15 13:56:46 +0800

    Update disk_space.py

--- script/monitor/ssh/disk_space.py
+++ script/monitor/ssh/disk_space.py
@@ -11,7 +11,7 @@ j = json.loads(x)
 
 def split_str(y):
     row = re.split(r"\s+", y)
-    return {"Filesystem": row[0], "Size": row[1], "Used": row[2], "Avail": row[3], "Ued%": row[4], "Mounted on": row[5], "timestamp": row[6]}
+    return {"Filesystem": row[0], "Size": row[1], "Used": row[2], "Avail": row[3], "Ued%": row[4], "Mounted on": row[5], "Timestamp": row[6]}
 
 g = list(map(split_str, j))
 data = json.dumps(g)
