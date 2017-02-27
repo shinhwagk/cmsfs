7339ebfb2fe357d492a8e81a5560683c55c4c57c
gk <shanghai_sl@hotmail.com>
2017-02-27 12:56:09 +0800

    test

--- backend/format-api/src/main/scala/org.wex.cmsfs.format.api/format/AnalyzeItem.scala
+++
@@ -1,9 +0,0 @@
-package org.wex.cmsfs.format.api.format
-
-import play.api.libs.json.{Format, Json}
-
-case class AnalyzeItem(detailId: Long, metricName: String, data: String, args: Seq[String])
-
-object AnalyzeItem {
-  implicit val format: Format[AnalyzeItem] = Json.format
-}
