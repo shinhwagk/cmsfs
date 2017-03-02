e2f38fe67f367d33fb4826f6e9f95d7a23eff37b
shinhwagk <shanghai_sl@hotmail.com>
2017-03-02 13:32:12 +0800

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,8 @@
 #  DATE: 2017 02 27                         #
 #  MAINTAINER: shinhwagk <191631513@qq.com> #
 #-------------------------------------------#
-BASE_HOME=`cd $(dirname $0); pwd`; cd $BASE_HOME
+DEPLOY_HOME=`cd $(dirname $0); pwd`;
+PROJECT_HOME=`cd ${DEPLOY_HOME}/../; pwd`
 
 function command_check() {
   command -v $1 >/dev/null 2>&1 || { echo >&2 "command: $1, no exist "; exit 1; }
@@ -19,17 +20,17 @@ function build_service() {
   sbt clean; sbt "${1}-impl"/universal:packageZipTarball || echo "${1}-impl build fail."; exit 1;
 }
 
-function build_all_service() {
-  sbt "${1}-impl/stage" || echo "build cmsfs fail."; exit 1;
-}
+# function build_all_service() {
+#   sbt "${1}-impl/stage" || echo "build cmsfs fail."; exit 1;
+# }
 
 function build_docker_image() {
   PATH="${1}/impl/target/universal/stage"
   docker build -t cmsfs/${1} --build-arg SVC_NAME=${1} --build-arg SVC_PATH=${PATH} .
 }
 
-function build_all_service() {
-  docker run -t --rm -v `pwd`:/opt/cmsfs -v /root/.ivy2:/root/.ivy2 sbt:0.13.13 sh -c "cd /opt/cmsfs; sbt clean; sbt stage"
+function package_all_service() {
+  docker run -t --rm -v ${PROJECT_HOME}:/opt/cmsfs -v /root/.ivy2:/root/.ivy2 sbt:0.13.13 sh -c "cd /opt/cmsfs; sbt clean; sbt stage"
 }
 
 function build_for_service() {
@@ -74,10 +75,13 @@ function process_args(){
   case "$1" in
     -h|-help)       help; exit 1 ;;
     --build)        build_for_service $2 ;;
-    --build-all)    build_all_service ;;
+    --package-all)  build_all_service ;;
     --start-all)    start_all_service ;;
     *)              help; exit 1;;
     esac
 }
 
+
+
+cd $DEPLOY_HOME;
 process_args $@
