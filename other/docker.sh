656b8962a98c569493961d90a131f022bc224fe1
shinhwagk <shanghai_sl@hotmail.com>
2017-03-03 10:52:17 +0800

    test

--- other/docker.sh
+++ other/docker.sh
@@ -5,9 +5,23 @@ docker images | grep none | awk '{print $3}' | while read line; do docker rmi $l
 docker images | grep none | awk '{print $3}' | while read line; do docker rmi $line; done 2>&1 | awk '{print $NF}' | while read line; do docker rm $line; done;
 docker images | grep none | awk '{print $3}' | while read line; do docker rmi $line; done
 docker images | grep none
+#  | wc -l
 
 # delete container for image name xxx
 docker ps -a | grep '[a-z0-9A-Z]\{12\}\s\{8\}[0-9]\{1\}' | awk '{print $1}' | while read line; do docker rm $line; done
 
 
 # delete container by image name
+
+delete_image() {
+  docker rmi $1
+}
+
+docker images | grep 'none' | awk '{print $3}' | while read image
+do
+  docker ps -a | grep $image | awk '{print $1}' | while read container
+  do
+    docker rm -f $container 
+  done
+done
+
