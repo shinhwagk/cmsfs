a42a5e1e44057187dfcc5cb822d252ff96808c81
gk <shanghai_sl@hotmail.com>
2017-03-13 17:29:21 +0800

    test

--- cmsfs/format-analyze/impl/src/main/scala/org/wex/cmsfs/format/analyze/impl/ServiceApplicationLoader.scala
+++ cmsfs/format-analyze/impl/src/main/scala/org/wex/cmsfs/format/analyze/impl/ServiceApplicationLoader.scala
@@ -1,13 +1,12 @@
 package org.wex.cmsfs.format.analyze.impl
 
-import com.lightbend.lagom.scaladsl.api.ServiceLocator
-import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
 import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
 import com.lightbend.lagom.scaladsl.pubsub.PubSubComponents
 import com.lightbend.lagom.scaladsl.server._
 import com.softwaremill.macwire._
 import org.wex.cmsfs.elasticsearch.api.ElasticsearchService
 import org.wex.cmsfs.format.analyze.api.FormatAnalyzeService
+import org.wex.cmsfs.lagom.service.discovery.consul.ConsulServiceLocatorComponents
 import play.api.LoggerConfigurator
 import play.api.libs.ws.ahc.AhcWSComponents
 
@@ -15,11 +14,9 @@ class ServiceApplicationLoader extends LagomApplicationLoader {
   override def loadDevMode(context: LagomApplicationContext): LagomApplication =
     new ServiceApplication(context) with LagomDevModeComponents
 
-  override def load(context: LagomApplicationContext): LagomApplication = {
+  override def load(context: LagomApplicationContext): LagomApplication = {{
     loaderEnvironment(context)
-    new ServiceApplication(context) {
-      override def serviceLocator: ServiceLocator = NoServiceLocator
-    }
+    new ServiceApplication(context) with ConsulServiceLocatorComponents
   }
 
   def loaderEnvironment(context: LagomApplicationContext): Unit = {
