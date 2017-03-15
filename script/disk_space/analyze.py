0e5ffd4517068bc4a12bae6a21ff2e29634e010a
gk <shanghai_sl@hotmail.com>
2017-03-15 14:02:18 +0800

    Update analyze.py

--- script/disk_space/analyze.py
+++ script/disk_space/analyze.py
@@ -2,7 +2,7 @@ import sys
 import json
 import re
 
-# "Filesystem" "Size" "Used" "Avail" "Use%" "Mounted on" "Timestamp"
+# "Filesystem" "Size" "Used" "Avail" "Use%" "Mounted on"
 
 f = open(sys.argv[1], "r")
 x = f.read()
@@ -13,7 +13,7 @@ j = json.loads(x)
 
 def split_str(y):
     row = re.split(r"\s+", y)
-    return {"Filesystem": row[0], "Size": int(row[1]), "Used": int(row[2]), "Avail": int(row[3]), "Ued%": int(row[4][:-1]), "Mounted on": row[5], "Timestamp": row[6]}
+    return {"Filesystem": row[0], "Size": int(row[1]), "Used": int(row[2]), "Avail": int(row[3]), "Ued%": int(row[4][:-1]), "Mounted on": row[5]}
 
 g = list(map(split_str, j))
 data = json.dumps(g)
