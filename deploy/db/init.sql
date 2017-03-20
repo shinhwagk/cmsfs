1d3b0a29ffb306eb75a4f13a97f227ece7a238dd
shinhwagk <shanghai_sl@hotmail.com>
2017-03-20 16:22:21 +0800

    test

--- deploy/db/init.sql
+++ deploy/db/init.sql
@@ -8,236 +8,126 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema oso_monitor
+-- Schema cmsfs
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `oso_monitor` ;
+DROP SCHEMA IF EXISTS `cmsfs` ;
 
 -- -----------------------------------------------------
--- Schema oso_monitor
+-- Schema cmsfs
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `oso_monitor` DEFAULT CHARACTER SET utf8 ;
-USE `oso_monitor` ;
+CREATE SCHEMA IF NOT EXISTS `cmsfs` DEFAULT CHARACTER SET utf8 ;
+USE `cmsfs` ;
 
 -- -----------------------------------------------------
--- Table `oso_monitor`.`MonitorAlarm`
+-- Table `cmsfs`.`core_collect`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`MonitorAlarm` (
-  `id` INT(11) NOT NULL,
-  `monitorId` VARCHAR(45) NOT NULL,
-  `args` JSON NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+DROP TABLE IF EXISTS `cmsfs`.`core_collect` ;
 
-
--- -----------------------------------------------------
--- Table `oso_monitor`.`alarms`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`alarms` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `script` TEXT NOT NULL,
-  `state` TINYINT(1) NOT NULL DEFAULT '1',
-  `args` JSON NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `oso_monitor`.`machine`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`machine` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `cmsfs`.`core_collect` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `tags` JSON NOT NULL,
-  `ip` VARCHAR(45) NOT NULL,
-  `state` TINYINT(1) NOT NULL,
+  `path` JSON NOT NULL,
+  `args` JSON NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `oso_monitor`.`connector_mode_jdbc`
+-- Table `cmsfs`.`core_connector_jdbc`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`connector_mode_jdbc` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `machine_id` INT(11) NOT NULL,
+DROP TABLE IF EXISTS `cmsfs`.`core_connector_jdbc` ;
+
+CREATE TABLE IF NOT EXISTS `cmsfs`.`core_connector_jdbc` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
   `url` TEXT NOT NULL,
   `user` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `tags` JSON NOT NULL,
-  `state` TINYINT(1) NOT NULL,
-  `category` VARCHAR(45) NOT NULL,
-  `category_version` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_jdbc_machine_id_idx` (`machine_id` ASC),
-  CONSTRAINT `fk_jdbc_machine_id`
-    FOREIGN KEY (`machine_id`)
-    REFERENCES `oso_monitor`.`machine` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `oso_monitor`.`connector_mode_ssh`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`connector_mode_ssh` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `machine_id` INT(11) NOT NULL,
-  `user` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NULL DEFAULT NULL,
-  `private_key` VARCHAR(45) NULL DEFAULT NULL,
-  `port` INT(11) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `tags` JSON NOT NULL,
-  `state` TINYINT(1) NULL DEFAULT NULL,
-  `category` VARCHAR(45) NOT NULL,
-  `category_version` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `machine_id_UNIQUE` (`machine_id` ASC),
-  INDEX `fk_ssh_machine_id_idx` (`machine_id` ASC),
-  CONSTRAINT `fk_ssh_machine_id`
-    FOREIGN KEY (`machine_id`)
-    REFERENCES `oso_monitor`.`machine` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `oso_monitor`.`groups`
+-- Table `cmsfs`.`core_connector_ssh`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`groups` (
-  `group_id` INT(11) NOT NULL COMMENT '一个组的所有用户，统一只能发送一个模式',
-  `group_name` VARCHAR(45) NOT NULL,
-  `send_mode` INT(11) NOT NULL,
-  PRIMARY KEY (`group_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+DROP TABLE IF EXISTS `cmsfs`.`core_connector_ssh` ;
 
-
--- -----------------------------------------------------
--- Table `oso_monitor`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `cmsfs`.`core_connector_ssh` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `group_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_group_id_idx` (`group_id` ASC),
-  CONSTRAINT `fk_group_id`
-    FOREIGN KEY (`group_id`)
-    REFERENCES `oso_monitor`.`groups` (`group_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `oso_monitor`.`email`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`email` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `address` VARCHAR(45) NOT NULL,
-  `user_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_user_id_idx` (`user_id` ASC),
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `oso_monitor`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `oso_monitor`.`metric`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`metric` (
-  `id` INT(11) NOT NULL,
-  `state` TINYINT(1) NOT NULL DEFAULT '1',
-  `name` VARCHAR(45) NOT NULL,
-  `mode` VARCHAR(45) NOT NULL,
-  `tags` JSON NOT NULL,
-  `cron` VARCHAR(45) NOT NULL,
-  `category` VARCHAR(45) NOT NULL,
-  `category_version` JSON NOT NULL,
-  `description` TEXT NOT NULL,
+  `ip` VARCHAR(45) NOT NULL,
+  `port` INT(11) NOT NULL,
+  `user` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NULL,
+  `private_key` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `oso_monitor`.`monitor_detail`
+-- Table `cmsfs`.`core_format_analyze`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`monitor_detail` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `metric_id` INT(11) NOT NULL,
-  `connector_id` INT(11) NOT NULL,
+DROP TABLE IF EXISTS `cmsfs`.`core_format_analyze` ;
+
+CREATE TABLE IF NOT EXISTS `cmsfs`.`core_format_analyze` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `path` JSON NOT NULL,
+  `args` JSON NULL,
+  `collect_id` INT(11) NOT NULL,
+  `_index` VARCHAR(45) NOT NULL,
+  `_type` VARCHAR(45) NOT NULL,
+  `_metric` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `cmsfs`.`core_monitor_detail`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cmsfs`.`core_monitor_detail` ;
+
+CREATE TABLE IF NOT EXISTS `cmsfs`.`core_monitor_detail` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `cron` VARCHAR(45) NOT NULL,
-  `collect_args` JSON NULL DEFAULT NULL,
-  `analyze_args` JSON NULL DEFAULT NULL,
-  `alarm` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_metric_id_idx` (`metric_id` ASC),
-  CONSTRAINT `fk_metric_id`
-    FOREIGN KEY (`metric_id`)
-    REFERENCES `oso_monitor`.`metric` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 45
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `oso_monitor`.`monitor_depository`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`monitor_depository` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `monitor_id` INT(11) NOT NULL,
-  `collect` JSON NOT NULL,
-  `analyze` JSON NULL DEFAULT NULL,
-  `alarm` JSON NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_collect_monitor_id_idx` (`monitor_id` ASC),
-  CONSTRAINT `fk_monitor_detail_id`
-    FOREIGN KEY (`monitor_id`)
-    REFERENCES `oso_monitor`.`monitor_detail` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `connector_mode` VARCHAR(45) NOT NULL,
+  `connector_id` INT(11) NOT NULL,
+  `collect_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `oso_monitor`.`phone`
+-- Table `cmsfs`.`core_format_alarm`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oso_monitor`.`phone` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `number` VARCHAR(45) NOT NULL,
-  `user_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_phone_user_id_idx` (`user_id` ASC),
-  CONSTRAINT `fk_phone_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `oso_monitor`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+DROP TABLE IF EXISTS `cmsfs`.`core_format_alarm` ;
+
+CREATE TABLE IF NOT EXISTS `cmsfs`.`core_format_alarm` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `path` JSON NOT NULL,
+  `args` JSON NULL,
+  `collect_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `cmsfs`.`core_connector_mongo`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cmsfs`.`core_connector_mongo` ;
+
+CREATE TABLE IF NOT EXISTS `cmsfs`.`core_connector_mongo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -246,54 +136,53 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `oso_monitor`.`machine`
+-- Data for table `cmsfs`.`core_collect`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `oso_monitor`;
-INSERT INTO `oso_monitor`.`machine` (`id`, `name`, `tags`, `ip`, `state`) VALUES (1, 'yali2', '[]', '10.65.193.25', 1);
-INSERT INTO `oso_monitor`.`machine` (`id`, `name`, `tags`, `ip`, `state`) VALUES (2, 'yali3', '[]', '10.65.193.26', 1);
-INSERT INTO `oso_monitor`.`machine` (`id`, `name`, `tags`, `ip`, `state`) VALUES (3, 'yali1', '[]', '10.65.212.193', 1);
+USE `cmsfs`;
+INSERT INTO `cmsfs`.`core_collect` (`id`, `name`, `path`, `args`) VALUES (1, 'disk_space', '[\"os\",\"centos\",\"common\",\"disk_space\",\"collect.sh\"]', NULL);
+INSERT INTO `cmsfs`.`core_collect` (`id`, `name`, `path`, `args`) VALUES (2, 'tablespace_space', '[\"oracle\",\"common\",\"tablespace_space\",\"collect.sql\"]', NULL);
+
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `oso_monitor`.`connector_mode_ssh`
+-- Data for table `cmsfs`.`core_connector_jdbc`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `oso_monitor`;
-INSERT INTO `oso_monitor`.`connector_mode_ssh` (`id`, `machine_id`, `user`, `password`, `private_key`, `port`, `name`, `tags`, `state`, `category`, `category_version`) VALUES (1, 1, 'oracle', NULL, NULL, 22, 'yali2', '[]', 1, 'CENTOS', '6');
-INSERT INTO `oso_monitor`.`connector_mode_ssh` (`id`, `machine_id`, `user`, `password`, `private_key`, `port`, `name`, `tags`, `state`, `category`, `category_version`) VALUES (2, 2, 'oracle', NULL, NULL, 22, 'yali3', '[]', 1, 'CENTOS', '6');
-INSERT INTO `oso_monitor`.`connector_mode_ssh` (`id`, `machine_id`, `user`, `password`, `private_key`, `port`, `name`, `tags`, `state`, `category`, `category_version`) VALUES (3, 3, 'oracle', NULL, NULL, 22, 'yali', '[]', 1, 'CENTOS', '6');
+USE `cmsfs`;
+INSERT INTO `cmsfs`.`core_connector_jdbc` (`id`, `category`, `name`, `url`, `user`, `password`) VALUES (1, DEFAULT, 'yali2', 'jdbc:oracle:thin:@10.65.193.25:1521/orayali', 'system', 'oracle');
+
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `oso_monitor`.`connector_mode_jdbc`
+-- Data for table `cmsfs`.`core_connector_ssh`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `oso_monitor`;
-INSERT INTO `oso_monitor`.`connector_mode_jdbc` (`id`, `machine_id`, `url`, `user`, `password`, `name`, `tags`, `state`, `category`, `category_version`) VALUES (1, 1, 'jdbc:oracle:thin:@10.65.193.25:1521/orayali2', 'system', 'oracle', 'yali2', '[]', 1, 'DATABASE', 'ORACLE');
+USE `cmsfs`;
+INSERT INTO `cmsfs`.`core_connector_ssh` (`id`, `name`, `ip`, `port`, `user`, `password`, `private_key`) VALUES (1, 'yali2', '10.65.193.25', 22, 'oracle', NULL, NULL);
+
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `oso_monitor`.`metric`
+-- Data for table `cmsfs`.`core_format_analyze`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `oso_monitor`;
-INSERT INTO `oso_monitor`.`metric` (`id`, `state`, `name`, `mode`, `tags`, `cron`, `category`, `category_version`, `description`) VALUES (1, 1, 'disk_space', 'SSH', '[]', '0/5 * * * * ?', 'CENTOS', '[]', '1111');
-INSERT INTO `oso_monitor`.`metric` (`id`, `state`, `name`, `mode`, `tags`, `cron`, `category`, `category_version`, `description`) VALUES (2, 1, 'tablespace_space', 'JDBC', '[]', '0/5 * * * * ?', 'DATABASE', '[]', '1111');
+USE `cmsfs`;
+INSERT INTO `cmsfs`.`core_format_analyze` (`id`, `path`, `args`, `collect_id`, `_index`, `_type`, `_metric`) VALUES (1, '[\"os\",\"centos\",\"common\",\"disk_space\"]', NULL, 1, 'monitor', 'os', 'disk_space');
+INSERT INTO `cmsfs`.`core_format_analyze` (`id`, `path`, `args`, `collect_id`, `_index`, `_type`, `_metric`) VALUES (2, '[\"oracle\",\"common\",\"tablespace_space\"]', NULL, 2, 'monitor', 'oracle', 'tablespace_space');
+
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `oso_monitor`.`monitor_detail`
+-- Data for table `cmsfs`.`core_monitor_detail`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `oso_monitor`;
-INSERT INTO `oso_monitor`.`monitor_detail` (`id`, `metric_id`, `connector_id`, `cron`, `collect_args`, `analyze_args`, `alarm`) VALUES (1, 1, 1, '0/5 * * * * ?', NULL, NULL, 1);
-INSERT INTO `oso_monitor`.`monitor_detail` (`id`, `metric_id`, `connector_id`, `cron`, `collect_args`, `analyze_args`, `alarm`) VALUES (2, 1, 2, '0/5 * * * * ?', NULL, NULL, 1);
-INSERT INTO `oso_monitor`.`monitor_detail` (`id`, `metric_id`, `connector_id`, `cron`, `collect_args`, `analyze_args`, `alarm`) VALUES (3, 1, 3, '0/5 * * * * ?', NULL, NULL, 1);
-INSERT INTO `oso_monitor`.`monitor_detail` (`id`, `metric_id`, `connector_id`, `cron`, `collect_args`, `analyze_args`, `alarm`) VALUES (4, 2, 1, '0/5 * * * * ?', NULL, NULL, 1);
+USE `cmsfs`;
+INSERT INTO `cmsfs`.`core_monitor_detail` (`id`, `cron`, `connector_mode`, `connector_id`, `collect_id`) VALUES (1, '0/5 * * * * ?', 'rdb', 1, 1);
+
 COMMIT;
 
