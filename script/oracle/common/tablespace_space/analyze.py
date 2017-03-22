f0fffbe6d866fbaa2339e5e0153be4cee6fa65c7
gk <shanghai_sl@hotmail.com>
2017-03-22 15:41:51 +0800

    Update analyze.py

--- script/oracle/common/tablespace_space/analyze.py
+++ script/oracle/common/tablespace_space/analyze.py
@@ -1,7 +1,21 @@
 import sys
+import json
 
 f = open(sys.argv[1], "r")
 x = f.read()
 f.close
+
+j = json.loads(x)
+
+def format_type(row):
+  row["USED_PCT"] = float(row["USED_PCT"])
+  row["FREE_MB"] = float(row["FREE_MB"])
+  row["USED_MB"] = float(row["USED_MB"])
+  row["TOTAL_MB"] = float(row["TOTAL_MB"])
+  return row
+
+g = list(map(format_type, j))
+
+data = json.dumps(g)
 
 print(x)
