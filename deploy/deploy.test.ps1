7a940eaddd17a06440932fc96bf66f95aedf98ea
shinhwagk <shanghai_sl@hotmail.com>
2017-04-05 15:36:09 +0800

    test

--- deploy/deploy.test.ps1
+++ deploy/deploy.test.ps1
@@ -28,6 +28,7 @@ function scp_cmsfs() {
   cd E:\github\cmsfs;
   git add -A
   git commit -m "test"
+  git push
   # scp -r -C E:\github\cmsfs root@10.65.103.63:/opt/cmsfs_test/
   # ssh root@10.65.103.63 "cd /opt/cmsfs_test/; git pull"
   # scp -r -C E:\github\deploy root@10.65.103.63:/opt/cmsfs_test/
