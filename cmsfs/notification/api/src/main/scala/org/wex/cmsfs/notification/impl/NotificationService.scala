78e142761016210ce751c1525673ddd48804bb5d
shinhwagk <shanghai_sl@hotmail.com>
2017-04-06 14:07:09 +0800

    test

--- cmsfs/notification/api/src/main/scala/org/wex/cmsfs/notification/impl/NotificationService.scala
+++ cmsfs/notification/api/src/main/scala/org/wex/cmsfs/notification/impl/NotificationService.scala
@@ -1,7 +1,12 @@
 package org.wex.cmsfs.notification.impl
 
-import com.lightbend.lagom.scaladsl.api.deser.MessageSerializer
+import akka.util.ByteString
+import com.lightbend.lagom.scaladsl.api.deser.{MessageSerializer, StrictMessageSerializer}
+import com.lightbend.lagom.scaladsl.api.deser.MessageSerializer.{NegotiatedDeserializer, NegotiatedSerializer}
+import com.lightbend.lagom.scaladsl.api.transport.{MessageProtocol, NotAcceptable, SerializationException, UnsupportedMediaType}
 import com.lightbend.lagom.scaladsl.api.{CircuitBreaker, Service, ServiceCall}
+
+import scala.collection.immutable.Seq
 
 object NotificationService {
   val SERVICE_NAME = "notification"
@@ -15,8 +20,43 @@ trait NotificationService extends Service {
     import NotificationService._
     import Service._
     named(SERVICE_NAME).withCalls(
-      pathCall("/mns-web/services/rest/msgNotify", pushNotificationItem)(MessageSerializer.StringMessageSerializer, MessageSerializer.StringMessageSerializer)
+      pathCall("/mns-web/services/rest/msgNotify", pushNotificationItem)
+      (new ProtobufSerializer, MessageSerializer.StringMessageSerializer)
         .withCircuitBreaker(CircuitBreaker.identifiedBy("alarm-circuitbreaker"))
     )
   }
+}
+
+class PlainTextSerializer(val charset: String) extends NegotiatedSerializer[String, ByteString] {
+  override val protocol = MessageProtocol(Some("application/x-www-form-urlencoded"), Some(charset))
+
+  override def serialize(message: String) = ByteString.fromString(message, charset)
+}
+
+class ProtobufSerializer extends StrictMessageSerializer[String] {
+
+  final private val serializer = {
+    new NegotiatedSerializer[String, ByteString]() {
+      override def protocol: MessageProtocol =
+        MessageProtocol(Some("application/x-www-form-urlencoded"))
+
+      def serialize(order: String) = {
+        ByteString.apply(order)
+      }
+    }
+  }
+
+  final private val deserializer = {
+    new NegotiatedDeserializer[String, ByteString] {
+      override def deserialize(bytes: ByteString) = ByteString.apply(bytes).toString()
+    }
+  }
+
+  override def serializerForRequest: NegotiatedSerializer[String, ByteString] = serializer
+
+  @scala.throws[UnsupportedMediaType]
+  override def deserializer(protocol: MessageProtocol): NegotiatedDeserializer[String, ByteString] = deserializer
+
+  @scala.throws[NotAcceptable]
+  override def serializerForResponse(acceptedMessageProtocols: Seq[MessageProtocol]): NegotiatedSerializer[String, ByteString] = serializer
 }
