ccb7da40c8874ec97c0017fc277f1f0be896e6fd
gk <shanghai_sl@hotmail.com>
2017-04-12 18:06:07 +0800

    Update analyze.py

--- script/os/centos/common/loadavg/analyze.py
+++ script/os/centos/common/loadavg/analyze.py
@@ -10,7 +10,7 @@ j = json.loads(x)
 
 def split_str(y):
     row = re.split(r"\s+", y)
-    return {"1m": float(row[0]), "2m": float(row[1]), "3m": float(row[2])}
+    return {"1m": float(row[0]), "5m": float(row[1]), "15m": float(row[2])}
 
 g = list(map(split_str, j))
 data = json.dumps(g)
