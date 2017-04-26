4d2bf9618cd3a4cb9c029d7740ab61049cd7a7f3
gk <shanghai_sl@hotmail.com>
2017-04-26 15:30:53 +0800

    Update alarm.py

--- script/os/centos/common/disk_space/alarm.py
+++ script/os/centos/common/disk_space/alarm.py
@@ -1,5 +1,30 @@
+import sys
 import json
+import re
 
-a = {"mail": "xx", "mobile" :"f"}
+# "Filesystem" "Size" "Used" "Avail" "Use%" "Mounted on"
 
-print(json.dumps(a))
+f = open(sys.argv[1], "r")
+x = f.read()
+f.close
+
+f2 = open(sys.argv[2], "r")
+x2 = f2.read()
+f2.close
+
+j = json.loads(x)
+
+
+def split_str(y):
+    row = re.split(r"\s+", y)
+    return {"Filesystem": row[0], "Size": int(row[1]), "Used": int(row[2]), "Avail": int(row[3]), "Ued%": int(row[4][:-1]), "Mounted on": row[5]}
+
+g = list(map(split_str, j))
+
+xr = filter(lambda x: x['Ued%'] >= int(11), g)
+
+rs = map(lambda y: {"mail": ("%s - %s%s") %
+                    (y["Filesystem"], y["Ued%"], "%"), "mobile": ("%s - %s%s") %
+                    (y["Filesystem"], y["Ued%"], "%")}, list(xr))
+data = json.dumps(list(rs))
+print(data)
