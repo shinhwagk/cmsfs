440fc6d88017be411cb21490012130ff5f775fb8
gk <shanghai_sl@hotmail.com>
2017-04-27 11:04:35 +0800

    Update collect.sh

--- script/oracle/common/bakcup_dump_check/collect.sh
+++ script/oracle/common/bakcup_dump_check/collect.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-LS='/bin/ls'
-
 bak_local="/bak_nfs"
+
+. /u03/tools/monitor/env
+
+if [[ ! -d ${bak_local} ]]; then
+  echo "${bak_local} dir no exist".
+  exit 0;
+fi
+
+LS='/bin/ls'
 
 today=`date "+%Y%m%d"`
 
 fileCnt=`${LS} ${bak_local} | grep -e "${today}.log" | wc -l`
 
-rs=""
+fileName="full_${hostname}_${today}.dmp"
+fileLogName="full_${hostname}_${today}.log"
 
 if [[ ${fileCnt} -gt 0 ]]; then
-  fileName=`${LS} ${bak_local} | grep -e "${today}.log"`
-  tail -n 1 "${bak_local}/${fileName}" | grep 'successfully' > /dev/null 2>&1
+  tail -n 1 "${bak_local}/${fileLogName}" | grep 'successfully' > /dev/null 2>&1
   if [[ $? == 0 ]]; then
-    rs="[\"${fileName}\",\"successfully\"]"
+    echo "${fileName}, successfully"
   else
-    rs="[\"${fileName}\",\"failure\"]"
+    echo "${fileName}, failure"
   fi
 else
-  rs="[\"${fileName}\",\"no exist\"]"
+  echo "${fileName}, no exist"
 fi
-
-echo "${rs}"
