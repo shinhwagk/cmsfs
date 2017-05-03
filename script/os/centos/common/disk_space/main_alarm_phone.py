7145b55cc064a5c1ba8697257ea4016c4270da62
gk <shanghai_sl@hotmail.com>
2017-05-03 17:01:14 +0800

    Update and rename main_alarm.py to main_alarm_phone.py

--- script/os/centos/common/disk_space/main_alarm_phone.py
+++ script/os/centos/common/disk_space/main_alarm_phone.py
@@ -8,8 +8,12 @@ f2 = open(sys.argv[2], "r")
 args = json.loads(f2.read())
 f2.close
 
+hostname = os.getenv("conn-name")
+host = os.getenv("conn-ip")
+
 filterData = filter(lambda x: x['Ued%'] >= args["threshold"], data)
 
 for d in filterData:
-    content = "%s - %s%s" % (d["Filesystem"], d["Ued%"], "%")
-    print(content)
+    content = "%s(%s) | %s - %s%s" % (hostname,host,d["Filesystem"], d["Ued%"], "%")
+    
+print(content)
