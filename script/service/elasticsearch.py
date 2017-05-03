8eda58f9b63bca5ad76732034613abbe0b52a3a6
gk <shanghai_sl@hotmail.com>
2017-05-03 17:34:48 +0800

    Update elasticsearch.py

--- script/service/elasticsearch.py
+++ script/service/elasticsearch.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import re
+import http
 
 f = open(sys.argv[1], "r")
 datas = json.loads(f.read())
@@ -10,11 +11,14 @@ f = open(sys.argv[2], "r")
 args = json.loads(f.read())
 f.close
 
-def sendElasticsearch(_index,_type,contents):
-  orderNo = int(time.time() * 1000)
-  for content in contents:
-    headers = {"Content-type": "application/json; charset=utf-8"}
-    conn = http.client.HTTPConnection("10.65.103.63", 9200)
-    conn.request("POST", "/mns-web/services/rest/msgNotify", content, headers)
-    response = conn.getresponse()
-    print(response.status, response.reason)
+
+def sendElasticsearch(_index, _type, contents):
+    for content in contents:
+        headers = {"Content-type": "application/json; charset=utf-8"}
+        conn = http.client.HTTPConnection("10.65.103.63", 9200)
+        url = "/%s/%s" % (_index, _type)
+        conn.request("POST", url, content, headers)
+        response = conn.getresponse()
+        print(response.status, response.reason)
+
+sendElasticsearch(args["_index"], args["_type"], datas)
