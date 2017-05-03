c0009d348a227fd9713fe54bebca9d419e80e971
gk <shanghai_sl@hotmail.com>
2017-05-03 13:11:46 +0800

    Update main_alarm.py

--- script/os/centos/common/disk_space/main_alarm.py
+++ script/os/centos/common/disk_space/main_alarm.py
@@ -8,7 +8,7 @@ f2 = open(sys.argv[2], "r")
 args = json.loads(f2.read())
 f2.close
 
-filterData = filter(lambda x: x['Ued%'] >= int(args), data)
+filterData = filter(lambda x: x['Ued%'] >= args["threshold"], data)
 
 for d in filterData:
     content = "%s - %s%s" % (d["Filesystem"], d["Ued%"], "%")
