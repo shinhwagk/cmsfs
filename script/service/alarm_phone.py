714dfe56715ab6ee9ae7c029cb8d4b68dbf2e2bf
gk <shanghai_sl@hotmail.com>
2017-05-03 16:52:51 +0800

    Update alarm_phone.py

--- script/service/alarm_phone.py
+++ script/service/alarm_phone.py
@@ -1,2 +1,12 @@
 from notice_phone import sendPhone
+import json
 
+f = open(sys.argv[1], "r")
+content = json.loads(f.read())
+f.close
+
+f = open(sys.argv[2], "r")
+phones = json.loads(f.read())
+f.close
+
+sendPhone(phones, content)
