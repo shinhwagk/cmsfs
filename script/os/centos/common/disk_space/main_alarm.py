52a57c254b9810c419c0c218386422e41b9f05d3
gk <shanghai_sl@hotmail.com>
2017-05-03 13:06:52 +0800

    Update main_alarm.py

--- script/os/centos/common/disk_space/main_alarm.py
+++ script/os/centos/common/disk_space/main_alarm.py
@@ -1,14 +1,15 @@
 from format_common import format_data
+import json
 import sys
 
 data = format_data()
 
 f2 = open(sys.argv[2], "r")
-args = f2.read()
+args = json.loads(f2.read())
 f2.close
 
 filterData = filter(lambda x: x['Ued%'] >= int(args), data)
 
 for d in filterData:
     content = "%s - %s%s" % (d["Filesystem"], d["Ued%"], "%")
-    mail_send("x", "disk_space", content)
+    print(content)
