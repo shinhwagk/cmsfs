afb1a79dc63746a4575041c27bfac313b07b0aeb
gk <shanghai_sl@hotmail.com>
2017-05-05 15:21:04 +0800

    Update main_elasticsearch.py

--- script/os/centos/common/disk_space/main_elasticsearch.py
+++ script/os/centos/common/disk_space/main_elasticsearch.py
@@ -4,11 +4,15 @@ import json
 
 es_timestamp = os.getenv("utc-date")
 es_metric = os.getenv("collect-name")
+es_host = os.getenv("conn-name")
+es_ip = os.getenv("conn-ip")
 
 datas = format_data()
 
 for data in datas:
   data['@timestamp'] = es_timestamp
   data['@metric'] = es_metric
+  data['@ip'] = es_ip
+  data['@host'] = es_host
   
 print(json.dumps(datas))
